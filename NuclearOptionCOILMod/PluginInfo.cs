960381454ec233db9613735fbfa8f974a33c256d
Modzer0 <brandon.r.jasper@gmail.com>
2026-02-28 21:39:52 -0500

    v0.56-beta: Fix ABM-L disappearing on mission load, persist through encyclopedia reload

--- NuclearOptionCOILMod/PluginInfo.cs
+++ NuclearOptionCOILMod/PluginInfo.cs
@@ -4,6 +4,6 @@ namespace NuclearOptionCOILMod
     {
         public const string PLUGIN_GUID = "com.nuclearoption.coil";
         public const string PLUGIN_NAME = "Nuclear Option COIL Laser-beta";
-        public const string PLUGIN_VERSION = "0.55.0";
+        public const string PLUGIN_VERSION = "0.56.0";
     }
 }
